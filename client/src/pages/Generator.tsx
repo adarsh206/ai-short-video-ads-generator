182dad8acadc531c65396dea322b2b4f5776541e
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-03-23 06:19:09 +0530

    Upload zone component added

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -1,8 +1,48 @@
+import { use, useState } from "react"
+import Title from "../components/Title"
+import UploadZone from "../components/UploadZone"
 
 
 const Generator = () => {
+
+    const [name, setName] = useState('');
+    const [productName, setProductName] =useState('');
+    const [productDescription, setProductDescription] = useState('');
+    const [aspectRatio, setAspectRatio] = useState('9:16');
+    const [productImage, setProductImage] = useState<File | null>(null);
+    const [modelImage, setModelImage] = useState<File | null>(null);
+    const [userPrompt, setUserPrompt] = useState('');
+    const [isGenerating, setIsGenerating] = useState(false);
+
+
+    const handleFileChange = (e : React.ChangeEvent<HTMLInputElement>, type: 'product' | 'model') => {
+        
+        if(e.target.files && e.target.files[0]){
+            if(type === 'product') setProductImage(e.target.files[0])
+        }
+    }
+
+
   return (
-    <div>Generator</div>
+    <div className="min-h-screen text-white p-6 md:p-12 mt-28">
+        <form className="max-w-4xl mx-auto mb-40">
+            <Title heading="Create In-Context Image" 
+                description="Upload your model and product images to generate stunning UGC, short-form videos and social media posts"
+            />
+
+            <div className="flex gap-20 max-sm:flex-col items-center justify-between">
+                {/* left col */}
+                <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
+                    <UploadZone label="Product Image" file={} onClear={} onChange={} />
+                </div>
+
+                {/* right col */}
+                <div>
+                    <p>Right Col</p>
+                </div>
+            </div>
+        </form>
+    </div>
   )
 }
 
