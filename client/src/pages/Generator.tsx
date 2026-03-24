1fc4db1b5f4f0bbba2bfce08f1449089372908f2
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-03-24 08:01:16 +0530

    forms added

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -18,27 +18,53 @@ const Generator = () => {
     const handleFileChange = (e : React.ChangeEvent<HTMLInputElement>, type: 'product' | 'model') => {
         
         if(e.target.files && e.target.files[0]){
-            if(type === 'product') setProductImage(e.target.files[0])
+            if(type === 'product') {
+                setProductImage(e.target.files[0])
+            }
+            else{
+                setModelImage(e.target.files[0]);
+            } 
         }
+    }
+
+    const handleGenerate = async (e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
     }
 
 
   return (
     <div className="min-h-screen text-white p-6 md:p-12 mt-28">
-        <form className="max-w-4xl mx-auto mb-40">
+        <form onSubmit={handleGenerate} className="max-w-4xl mx-auto mb-40">
             <Title heading="Create In-Context Image" 
                 description="Upload your model and product images to generate stunning UGC, short-form videos and social media posts"
             />
 
-            <div className="flex gap-20 max-sm:flex-col items-center justify-between">
+            <div className="flex gap-20 max-sm:flex-col items-start justify-between">
                 {/* left col */}
                 <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
-                    <UploadZone label="Product Image" file={} onClear={} onChange={} />
+                    <UploadZone label="Product Image" file={productImage} onClear={() => setProductImage(null)} onChange={(e) => handleFileChange(e, 'product')} />
+                    <UploadZone label="Model Image" file={modelImage} onClear={() => setModelImage(null)} onChange={(e) => handleFileChange(e, 'model')} />
                 </div>
 
                 {/* right col */}
-                <div>
-                    <p>Right Col</p>
+                <div className="w-full">
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block text-sm mb-4">Project Name</label>
+                        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name your project" required
+                        className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all"/>
+                    </div>
+
+                    <div className="mb-4 text-gray-300">
+                        <label htmlFor="productName" className="block text-sm mb-4">Product Name</label>
+                        <input type="text" id="productName" value={productName} onChange={(e) => setProductName(e.target.value)} placeholder="Enter the name of the product" required
+                        className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all"/>
+                    </div>
+
+                     <div className="mb-4 text-gray-300">
+                        <label htmlFor="productDescription" className="block text-sm mb-4">Product Description <span className="text-xs text-violet-400">(optional)</span></label>
+                        <textarea id="productDescription" rows={4} value={productDescription} onChange={(e) => setProductDescription(e.target.value)} placeholder="Enter the description of the product"
+                            className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all"/>
+                    </div>
                 </div>
             </div>
         </form>
