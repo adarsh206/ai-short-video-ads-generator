e10ca2c36c6fa0cefa19dac0e36264afa286646f
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-03-30 07:11:05 +0530

    Result Page started

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -1,8 +1,67 @@
+import { useEffect, useState } from "react"
+import type { Project } from "../types"
+import { dummyGenerations } from "../assets/assets";
+import { Loader2Icon, RefreshCwIcon } from "lucide-react";
+import { Link } from "react-router-dom";
 
 
 const Result = () => {
-  return (
-    <div>Result</div>
+
+  const [project, setProjectData] = useState<Project>({} as Project);
+  const [loading, setLoading] = useState(true);
+  const [isGenerating, setIsGenerating] = useState(true);
+  
+
+  const fetchProjectData = async () => {
+    setTimeout(() => {
+      setProjectData(dummyGenerations[0]);
+      setLoading(false)
+    }, 3000)
+  }
+
+  useEffect(() => {
+    fetchProjectData()
+  }, [])
+
+  return loading ? (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Loader2Icon className="animate-spin text-indigo-500 size-9"/>
+    </div>
+  ) : (
+    <div className="min-h-screen text-white p-6 md:p-12 mt-20">
+      <div className="max-w-6xl mx-auto">
+        <header className="flex justify-between items-center mb-8">
+          <h1 className="text-2xl md:text-3xl font-medium">Generation Result</h1>
+          <Link to='/generate' className="btn-secondary text-sm flex items-center gap-2">
+            <RefreshCwIcon className="w-4 h-4" />
+            <p className="max-sm:hidden">New Generation</p>
+          </Link>
+        </header>
+
+        {/* grid layout */}
+        <div className="grid lg:grid-cols-3 gap-8">
+          {/* Main Result Display */}
+          <div className="lg:col-span-2 space-y-6">
+            <div className="glass-panel inline-block p-2 rounded-2xl">
+              <div>
+                {
+                  project?.generatedVideo ? (
+                    <video src={project.generatedVideo} controls autoPlay loop className="w-full h-full object-cover"/>
+                  ) : (
+                    <img src={project.generatedImage} alt="Generated Result" className="w-full h-full object-cover"/>
+                  )
+                }
+              </div>
+            </div>
+          </div>
+
+          {/* Sidebar Actions */}
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
