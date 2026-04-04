4bc79990fa83243b6a1de4d7e1fa96d402d131be
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-04-04 18:48:32 +0530

    neonDB and prisma setup

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from 'cors'
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware())
 
 const PORT = process.env.PORT || 5000;
 
